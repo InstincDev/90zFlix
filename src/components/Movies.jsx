0dd8dbc885dd0ca7bb40cc0d0fd11276a2a25603
InstincDev <crene7722@gmail.com>
2024-12-14 15:33:52 -0600

    fixed movie img bug

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,31 +1,22 @@
-import React, {useState} from "react"
-
+import React from "react"
+import {movies} from "../movies"
 
 function Movies() {
-    const [thumbnails, setThumbnails] = useState([
-        {id: 1, title: "Title 1", img: "placeholder.jpg"},
-        {id: 2, title: "Title 2", img: "placeholder.jpg"},
-        {id: 3, title: "Title 3", img: "placeholder.jpg"},
-        {id: 4, title: "Title 4", img: "placeholder.jpg"},
-        {id: 5, title: "Title 5", img: "placeholder.jpg"},
-        {id: 6, title: "Title 6", img: "placeholder.jpg"},
-        {id: 7, title: "Title 7", img: "placeholder.jpg"},
-        {id: 8, title: "Title 8", img: "placeholder.jpg"},
-        {id: 9, title: "Title 9", img: "placeholder.jpg"},
-        {id: 10, title: "Title 10", img: "placeholder.jpg"},
-    ])
 
     return(
         <div className="content">
             <table>
                 <tbody>
-                    {thumbnails.map((thumb, index) => (
-                    <tr key={thumb.id}>
-                        <td>
-                            <img src="{thumb.img}" alt="{thumb.title}" />
-                            <p>{thumb.title}</p>
+                    {movies.map((movie, i) => (
+                    <tr key={movie.year}>
+                        <td className="movie-card">
+                            <img src={movie.image} alt={movie.title} className="movie-img" />
+                            <div className="movie-info">
+                                <h3>{movie.title} ({movie.year})</h3>
+                            </div>
+                            <p>{movie.description}</p>
                         </td>
-                        {index % 3 === 2}
+                        {i % 3 === 2}
                     </tr>
                 ))}
                 </tbody>
--- src/movies.js
+++ src/movies.js
@@ -1,63 +1,75 @@
+// import homeAlone from './assets/home_alone.jpg';
+// import terminator2 from './assets/terminator.jpg';
+// import aladdin from './assets/aladdin1992.jpeg';
+// import jurassicPark from './assets/jurassic_park.jpg';
+// import lionKing from './assets/thelionking.jpeg';
+// import toyStory from './assets/Toy_Story.jpg';
+// import independenceDay from './assets/independence_day.jpg';
+// import titanic from './assets/Titanic.png';
+// import savingPrivateRyan from './assets/Saving_Private_Ryan.jpg';
+// import matrix from './assets/The-Matrix.jpg';
+
+
 export const movies = [
     {
       year: 1990,
       title: "Home Alone",
       description: "A boy defends his home from burglars during Christmas.",
-      image: "https://m.media-amazon.com/images/I/81Cgsoqf4wL._AC_SY679_.jpg",
+      image: "/home_alone.jpg",
     },
     {
       year: 1991,
       title: "Terminator 2: Judgment Day",
       description: "A cyborg protects the future of humanity.",
-      image: "https://m.media-amazon.com/images/I/71R+gJ3WQeL._AC_SY679_.jpg",
+      image: "/terminator.jpg",
     },
     {
       year: 1992,
       title: "Aladdin",
       description: "A street rat discovers a magical lamp and a Genie.",
-      image: "https://m.media-amazon.com/images/I/91pRXA3zeYL._AC_SY679_.jpg",
+      image: "/aladdin1992.jpeg",
     },
     {
       year: 1993,
       title: "Jurassic Park",
       description: "Dinosaurs brought back to life in a thrilling adventure.",
-      image: "https://m.media-amazon.com/images/I/81VfBY8AK-L._AC_SY679_.jpg",
+      image: "/jurassic_park.jpg",
     },
     {
       year: 1994,
       title: "The Lion King",
       description: "A lion cub’s journey to become king of the Pride Lands.",
-      image: "https://m.media-amazon.com/images/I/71mB5k9Xw6L._AC_SY679_.jpg",
+      image: "/thelionking.jpeg",
     },
     {
       year: 1995,
       title: "Toy Story",
       description: "The first computer-animated feature film about living toys.",
-      image: "https://m.media-amazon.com/images/I/91N1kHApUkL._AC_SY679_.jpg",
+      image: "/Toy_Story.jpg",
     },
     {
       year: 1996,
       title: "Independence Day",
       description: "Aliens attack Earth, and humanity fights back.",
-      image: "https://m.media-amazon.com/images/I/91nJDzApPFL._AC_SY679_.jpg",
+      image: "/independence_day.jpg",
     },
     {
       year: 1997,
       title: "Titanic",
       description: "A romantic epic set during the Titanic’s maiden voyage.",
-      image: "https://m.media-amazon.com/images/I/71c05lTE03L._AC_SY679_.jpg",
+     image: "/Titanic.png",
     },
     {
       year: 1998,
       title: "Saving Private Ryan",
       description: "A gripping World War II story about a rescue mission.",
-      image: "https://m.media-amazon.com/images/I/71j8oCMyMZL._AC_SY679_.jpg",
+     image: "/Saving_Private_Ryan.jpg",
     },
     {
       year: 1999,
       title: "The Matrix",
       description: "A hacker discovers the truth about reality.",
-      image: "https://m.media-amazon.com/images/I/51vpnbwFHrL._AC_.jpg",
+    image: "/The-Matrix.jpg",
     },
   ];
   
