82eb458adbdb4d80557e65d97a63166c44eb399c
InstincDev <crene7722@gmail.com>
2024-12-14 13:54:30 -0600

    add movie data

---
+++ src/assets/movies.js
@@ -0,0 +1,63 @@
+export const movies = [
+    {
+      year: 1990,
+      title: "Home Alone",
+      description: "A boy defends his home from burglars during Christmas.",
+      image: "https://m.media-amazon.com/images/I/81Cgsoqf4wL._AC_SY679_.jpg",
+    },
+    {
+      year: 1991,
+      title: "Terminator 2: Judgment Day",
+      description: "A cyborg protects the future of humanity.",
+      image: "https://m.media-amazon.com/images/I/71R+gJ3WQeL._AC_SY679_.jpg",
+    },
+    {
+      year: 1992,
+      title: "Aladdin",
+      description: "A street rat discovers a magical lamp and a Genie.",
+      image: "https://m.media-amazon.com/images/I/91pRXA3zeYL._AC_SY679_.jpg",
+    },
+    {
+      year: 1993,
+      title: "Jurassic Park",
+      description: "Dinosaurs brought back to life in a thrilling adventure.",
+      image: "https://m.media-amazon.com/images/I/81VfBY8AK-L._AC_SY679_.jpg",
+    },
+    {
+      year: 1994,
+      title: "The Lion King",
+      description: "A lion cub’s journey to become king of the Pride Lands.",
+      image: "https://m.media-amazon.com/images/I/71mB5k9Xw6L._AC_SY679_.jpg",
+    },
+    {
+      year: 1995,
+      title: "Toy Story",
+      description: "The first computer-animated feature film about living toys.",
+      image: "https://m.media-amazon.com/images/I/91N1kHApUkL._AC_SY679_.jpg",
+    },
+    {
+      year: 1996,
+      title: "Independence Day",
+      description: "Aliens attack Earth, and humanity fights back.",
+      image: "https://m.media-amazon.com/images/I/91nJDzApPFL._AC_SY679_.jpg",
+    },
+    {
+      year: 1997,
+      title: "Titanic",
+      description: "A romantic epic set during the Titanic’s maiden voyage.",
+      image: "https://m.media-amazon.com/images/I/71c05lTE03L._AC_SY679_.jpg",
+    },
+    {
+      year: 1998,
+      title: "Saving Private Ryan",
+      description: "A gripping World War II story about a rescue mission.",
+      image: "https://m.media-amazon.com/images/I/71j8oCMyMZL._AC_SY679_.jpg",
+    },
+    {
+      year: 1999,
+      title: "The Matrix",
+      description: "A hacker discovers the truth about reality.",
+      image: "https://m.media-amazon.com/images/I/51vpnbwFHrL._AC_.jpg",
+    },
+  ];
+  
